70b8455b66ef4f5d427e96e0f8b75566425bf2fa
subir <subir@Madeye>
2016-11-21 16:12:26 -0700

    Added prints

--- P2PFileSharing/src/com/p2p/ui/HomeScreen.java
+++ P2PFileSharing/src/com/p2p/ui/HomeScreen.java
@@ -72,7 +72,7 @@ public class HomeScreen {
 
 		lblStatus.setBounds(102, 250, 417, 23);
 		lblStatus.setText("Status");
-		
+		System.out.println("About to start");
 		if("bootPeer".equalsIgnoreCase("bootPeer")){
 			P2PControllerBootPeer.MakePeer();
 		}else{
